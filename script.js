87c44402da1e3f9332d23c2254b0019c04242267
Christina Asquith <christinaasquith@gmail.com>
2020-12-24 09:16:59 -0500

    finish workshop and add javascript to spin the wheel
    there is still a problem w the wheel not broken into triangles

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function rotateFunction(){
+    var min = 1024;
+    var max = 9999;
+    var deg = Math.floor(Math.random() * (max - min)) + min; document.getElementById('box').style.transform = "rotate("+deg+"deg)"; 
+    }
+
+    setTimeout(function(){
+        Element.classList.add('animate');
+    }, 5000);
+     
